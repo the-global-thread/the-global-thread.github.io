3c2b80b8c740240235990ca99d78c32a6618ab75
Mahdi Bandegani <mahdibandegani@gmail.com>
2026-02-14 00:41:48 +0330

    refactor: move repo a folder level inside

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd /Users/mahdibnd/Developer/News/site
-npx wrangler pages deploy . --project-name irannews --branch main --commit-hash "$(date +%s)" --commit-message "manual redeploy" --commit-dirty=true
